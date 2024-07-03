89be253b72cf1de756e1d18179f0d5361f7e1a6b
Dongjin <triumph0510@gmail.com>
2024-07-03 11:06:10 +0900

    [feature] page 01

--- Sample-Test1/prime-factors_test.cpp
+++ Sample-Test1/prime-factors_test.cpp
@@ -1,9 +1,16 @@
 #include "pch.h"
 #include "../TDD_PrimeFactors/prime-factors.cpp"
 
+#include <vector>
+
 using namespace std;
 
-TEST(TestCaseName, TestName) {
-	EXPECT_EQ(1, 1);
-	EXPECT_TRUE(true);
+class PrimeFactors : public testing::Test {
+
+};
+
+TEST(PrimeFactors, Of1) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
 }
